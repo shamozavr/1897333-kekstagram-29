86272822b2b8cddf834f1d512aeb344c03c95525
Roman Shashin <propeller1992@gmail.com>
2023-06-12 19:22:07 +0300

    исправил numExtract для возвращения числа а не строки

--- js/functions.js
+++ js/functions.js
@@ -4,4 +4,4 @@ const polyndromeCheck = (str) =>
   str.replace(/\s/g, '').toLowerCase() ===
   str.replace(/\s/g, '').toLowerCase().split('').reverse().join('');
 
-const numExtract = (str) => str.toString().match(/\d+/g) === null ? NaN : str.toString().match(/\d+/g).join('');
+const numExtract = (str) => str.toString().match(/\d+/g) === null ? NaN : Number(str.toString().match(/\d+/g).join(''));
