e6007192a80edafd1b21296fc1c3460db1f3b802
Roman Shashin <propeller1992@gmail.com>
2023-06-12 12:09:28 +0300

    функции для ДЗ 2 модуля

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const lengthFN = (str, maxLength) => str.length <= maxLength;
+
+const polyndromeCheck = (str) =>
+  str.replace(/\s/g, '').toLowerCase() ===
+  str.replace(/\s/g, '').toLowerCase().split('').reverse().join('');
+
+const numExtract = (str) => str.toString().match(/\d+/g) === null ? NaN : str.toString().match(/\d+/g).join('');
