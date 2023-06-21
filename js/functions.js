ca199ef0b93e071efd3ea9a8ac7a40b414e4c0eb
Roman Shashin <propeller1992@gmail.com>
2023-06-21 14:25:05 +0300

    Функция сравнения времени

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,23 @@ const polyndromeCheck = (str) =>
   str.replace(/\s/g, '').toLowerCase().split('').reverse().join('');
 
 const numExtract = (str) => str.toString().match(/\d+/g) === null ? NaN : Number(str.toString().match(/\d+/g).join(''));
+
+const isTimeOk = (dayStart, dayEnd, MeetingStart, MeetingDuration) => {
+  function turnIntoMin(time) {
+    const splittedTime = time.split(':');
+    return Number(splittedTime[0]) * 60 + Number(splittedTime[1]);
+  }
+  const dayStartMin = turnIntoMin(dayStart);
+  const dayEndMin = turnIntoMin(dayEnd);
+  const MeetingStartMin = turnIntoMin(MeetingStart);
+
+  return (
+    MeetingStartMin + MeetingDuration <= dayEndMin &&
+    MeetingStartMin >= dayStartMin
+  );
+};
+
 lengthFN();
 polyndromeCheck();
 numExtract();
+isTimeOk();
