2ef31851000e50f92790b573c773af9d71323da0
Roman Shashin <propeller1992@gmail.com>
2023-07-12 19:12:27 +0300

    feat: limit ammount of comments showed

--- js/bigpicture.js
+++ js/bigpicture.js
@@ -1,11 +1,14 @@
 import {isEscapeKey} from './utils.js';
 
+const COMMENTS_PER_PORTION = 5;
+let commentsShown = 5;
+
 const bigPicture = document.querySelector('.big-picture'); // блок с полноразмерным изображением
 const bigPictureImgBlock = bigPicture.querySelector('.big-picture__img'); //изображение блока bigPicture
 const bigPictureImg = bigPictureImgBlock.querySelector('img'); //изображение блока bigPicture
 const bigPictureLikes = bigPicture.querySelector('.likes-count'); //Количество лайков likes
-const bigPictureCommentsCount = bigPicture.querySelector('.comments-count'); //Количество комментариев comments
-const bigPictureSocialCommentsCount = bigPicture.querySelector('.social__comment-count'); //Количество комментариев comments
+const bigPictureCommentsCount = bigPicture.querySelector('.comments-count'); //Общее количество комментариев comments
+const bigPictureSocialCommentsCount = bigPicture.querySelector('.social__comment-count'); //Показанное количество комментариев comments
 const commentsLoader = document.querySelector('.comments-loader'); //кнопка загрузки комментариев
 const bigPictureCloseButton = bigPicture.querySelector('.big-picture__cancel'); //кнопка закрытия модалки
 
@@ -13,10 +16,20 @@ const commentsList = document.querySelector('.social__comments');//Список 
 const newCommentTemplate = document.querySelector('.social__comment');//шаблон комментария для наполнения
 const CommentsFragment = document.createDocumentFragment();//Фрагмент для наполнения и дальнейше вставки
 
+
 //функция отрисовки комментариев которая принимает в себя массив объектов
 const renderComments = (arrayOfComments) => {//arrayOfComments, каждый объект которого имеет ключи с именами avatar, message, name
   commentsList.innerHTML = '';
-  arrayOfComments.forEach(({avatar, message, name}) => {//при помощи деструктуризации передаем их в качетсве аргументов в forEach
+
+  const slicedArray = arrayOfComments.slice(0, commentsShown);
+  if (arrayOfComments.length <= slicedArray.length) {
+    commentsLoader.classList.add('hidden');
+  }
+
+  bigPictureSocialCommentsCount.textContent = `${commentsShown} из`;
+  bigPictureCommentsCount.textContent = arrayOfComments.length;
+
+  slicedArray.forEach(({avatar, message, name}) => {//при помощи деструктуризации передаем их в качетсве аргументов в forEach
     const comment = newCommentTemplate.cloneNode(true);//Копируем шаблон для наполнения на каждой итерации forEach
     const commentImg = comment.querySelector('img');//находим аватарку комментатора
 
@@ -38,13 +51,16 @@ const renderBigPicture = ({url, likes, comments}) => {
   bigPictureCommentsCount.textContent = comments.length;
 
   renderComments (comments);
+  commentsLoader.addEventListener('click', () => {
+    commentsShown += COMMENTS_PER_PORTION;
+
+    renderComments (comments);
+  });
 };
 
 //функция openBigPicture, которая предназанчена для того чтобы:
 const openBigPicture = (element) => {
   bigPicture.classList.remove('hidden');//показать модалку BigPicture, убрав класс hidden
-  bigPictureSocialCommentsCount.classList.add('hidden');//Скрыть счетчик комментариев, добавив класс hidden
-  commentsLoader.classList.add('hidden');//Скрыть кнопку загрузки комментариев, добавив класс hidden
   document.documentElement.classList.add('modal-open');//Добавить класс modal-open на <body>
 
   renderBigPicture (element);//вызов функции отрисовки BigPicture на основе полученных данных
