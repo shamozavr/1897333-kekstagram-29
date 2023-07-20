974c5eceb4553c098c839c95f09115030fba6241
Шашин Роман <propeller1992@gmail.com>
2023-07-20 14:45:43 +0300

    feat: slider hide&show functions added

--- js/effects.js
+++ js/effects.js
@@ -1,10 +1,3 @@
-// const sliderContainer = document.querySelector('.img-upload__effect-level');
-// const sliderElement = document.querySelector('.effect-level__slider');
-// const effectsContainer = document.querySelector('.img-upload__effects');
-// const effectValue = document.querySelector('.effect-level__value');
-// const allEffects = document.querySelectorAll('.effects__radio');
-
-const modalElement = document.querySelector('.img-upload');
 const imageElement = document.querySelector('.img-upload__preview img');
 const sliderContainer = document.querySelector('.img-upload__effect-level');
 const sliderElement = document.querySelector('.effect-level__slider');
@@ -62,7 +55,6 @@ const EFFECTS = {
   }
 };
 
-
 let chosenEffect = EFFECTS.none;
 
 const showSlider = () => {
@@ -91,7 +83,6 @@ const setImageStyle = () => {
 
 effectsContainer.addEventListener('click', (evt) => {
   if (evt.target.value && evt.target.value !== 'none') {
-    console.log(evt.target.value);
     showSlider();
     chosenEffect = EFFECTS[evt.target.value];
     sliderElement.noUiSlider.updateOptions({
@@ -112,73 +103,3 @@ sliderElement.noUiSlider.on('update', () => {
   effectValue.value = sliderElement.noUiSlider.get();
   setImageStyle();
 });
-
-
-// const setEffect = (effect) => {
-//   chosenEffect = effect;
-//   setSlider(effect);
-// };
-
-// const onEffectChange = (evt) => {
-//   setEffect(evt.target.value);
-// };
-
-// const reset = () => {
-//   setEffect (EFFECTS.none);
-// };
-
-// const createSlider = (effect) => {
-//   noUiSlider.create(sliderElement, {
-//     range: {
-//       min: EFFECTS.effect.min,
-//       max: EFFECTS.effect.max,
-//     },
-//     start: (EFFECTS.effect.max - EFFECTS.effect.min) / 2,
-//     step: EFFECTS.effect.step,
-//     connect: 'lower',
-//   });
-// };
-
-// const setSlider = (effect) => {
-//   sliderContainer.noUiSlider.destroySlider;
-//   sliderContainer.classList.add('hidden');
-
-//   if (chosenEffect !== EFFECTS.none) {
-//     createSlider(effect);
-//     sliderContainer.classList.remove('hidden');
-//   }
-// };
-
-// onSliderUpdate = () => {
-//   sliderElement.noUiSlider.on('update', () => {
-//     effectValue.value = sliderElement.noUiSlider.get();
-//   });
-//   setImageStyle();
-// };
-
-// const setImageStyle = (element) => {
-//   document.querySelector(`#effect-${element}`).style.filter = `${EFFECTS.element.filter}(${effectValue.value}${EFFECTS.element.unit})`;
-// };
-
-// onSliderUpdate();
-// setSlider();
-
-
-// const effectCheck = (effectsArray, effectsObjects) => {
-//   effectsArray.forEach((element) => {
-//     element.addEventListener('change', (evt) => {
-//       if (evt.target.checked) {
-//         sliderElement.noUiSlider.updateOptions({
-//           range: {
-//             min: effectsObjects.element.filter.min,
-//             max: effectsObjects.element.filter.max,
-//           },
-//           step: effectsObjects.element.step,
-//         });
-
-//       }
-//     });
-//   });
-// };
-
-// effectCheck(allEffects, EFFECTS);
